fc29b6b1e1761191aea48b706564a59a4049f14b
David Holmes <davidholmesnyc@gmail.com>
2015-05-31 18:50:17 -0400

    forgot space

--- js/demo.js
+++ js/demo.js
@@ -2,7 +2,7 @@ $(function(){
   if(config.EMAIL_LIMIT < 0 || config.EMAIL_LIMIT > 0 ){
 
     var html  = '<div class="alert alert-warning demo-warning">'
-        html +='Demo only grabs last '+Math.abs(config.EMAIL_LIMIT)+'emails because of privacy,storage and performance concerns.'
+        html +='Demo only grabs last '+Math.abs(config.EMAIL_LIMIT)+' emails because of privacy,storage and performance concerns.'
         html +='<br/>'
         html +='For all emails please <a href="https://github.com/davidholmesnyc/dmail" target="_blank">Download Dmail for free on Github</a>'
         html +='</div>'
